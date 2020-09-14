835ac719f5b979fd213e70d4f642e14fea1fc09a
Mahmoud Aboelsoud <mahmoud.aboelsoud.p@gmail.com>
2020-09-14 20:13:17 +0200

    3-practical app

--- 3-practical app/3.php
+++ 3-practical app/3.php
@@ -1,38 +1,82 @@
 <?php include "functions.php" ?>
 <?php include "includes/header.php" ?>
 
-	<section class="content">
+<section class="content">
 
 	<aside class="col-xs-4">
 
-	<?php Navigation();?>
-			
-	</aside><!--SIDEBAR-->
+		<?php Navigation(); ?>
+
+	</aside>
+	<!--SIDEBAR-->
 
 
-<article class="main-content col-xs-8">
+	<article class="main-content col-xs-8">
 
-<?php  
+		<?php
 
-/*  Step1: Make an if Statement with elseif and else to finally display string saying, I love PHP
+		/*  Step1: Make an if Statement with elseif and else to finally display string saying, I love PHP
 
+			Step 2: Make a forloop  that displays 10 numbers
 
-
-	Step 2: Make a forloop  that displays 10 numbers
-
-
-	Step 3 : Make a switch Statement that test againts one condition with 5 cases
-
- */
-
-	
-?>
+			Step 3 : Make a switch Statement that test againts one condition with 5 cases
+		*/
 
 
+		if (false) {
+			echo "NONE in if";
+		} elseif (false) {
+			echo "NONE in elseif";
+		} else {
+			echo "I love php !";
+		}
+
+		echo "<br>";
+
+		for ($i = 10; $i <= 100; $i += 10) {
+			echo $i . "  ";
+		}
+
+		echo "<br>";
+
+		$number = 3;
+
+		switch ($number) {
+
+			case 1:
+				echo 1;
+				break;
+
+			case 2:
+				echo 2;
+				break;
+
+			case 3:
+				echo 3;
+				break;
+
+			case 4:
+				echo 4;
+				break;
+
+			case 5:
+				echo 5;
+				break;
+
+			default:
+				echo "UNDEFINED";
+				break;
+		}
+
+
+		?>
 
 
 
 
-</article><!--MAIN CONTENT-->
-	
-<?php include "includes/footer.php" ?>
+
+
+	</article>
+	<!--MAIN CONTENT-->
+
+	<?php include "includes/footer.php" ?>
