5812dbce6d7494077d460c79f3b67839b51f8579
Mahmoud Aboelsoud <mahmoud.aboelsoud.p@gmail.com>
2020-09-14 19:38:03 +0200

    10-switch

---
+++ 10-switch/switch.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8>">
+    <title>Switch statements</title>
+</head>
+
+<body>
+
+    <?php
+
+    echo "<h1>";
+
+
+    $age = 20;
+
+    switch ($age) {
+
+        case 20:
+            echo "You are 20 !";
+            break;
+
+        case 25;
+            echo "You are 25 !";
+            break;
+
+        default:
+            echo "UNDEFINED";
+            break;
+    }
+
+
+
+
+
+
+    echo "</h1>";
+    ?>
+
+
+</body>
+
+</html>
