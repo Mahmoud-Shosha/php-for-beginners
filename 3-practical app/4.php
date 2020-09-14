cbd90d870a25f806bd606569f1b0b68ebcd34c57
Mahmoud Aboelsoud <mahmoud.aboelsoud.p@gmail.com>
2020-09-14 21:32:56 +0200

    3-practical app

--- 3-practical app/4.php
+++ 3-practical app/4.php
@@ -1,36 +1,44 @@
 <?php include "functions.php" ?>
 <?php include "includes/header.php" ?>
 
-	<section class="content">
+<section class="content">
 
 	<aside class="col-xs-4">
 
-		<?php Navigation();?>
-			
-		
-	</aside><!--SIDEBAR-->
+		<?php Navigation(); ?>
 
 
-<article class="main-content col-xs-8">
-
-	
-	<?php  
-
-/*  Step1: Define a function and make it return a calculation of 2 numbers
-
-	Step 2: Make a function that passes parameters and call it using parameter values
+	</aside>
+	<!--SIDEBAR-->
 
 
- */
-
-	
-?>
+	<article class="main-content col-xs-8">
 
 
+		<?php
+
+		/*  Step1: Define a function and make it return a calculation of 2 numbers
+
+			Step 2: Make a function that passes parameters and call it using parameter values
+
+		*/
+
+		echo add(5, 22);
+
+		function add($num1, $num2)
+		{
+			return $num1 + $num2;
+		}
+
+
+		?>
 
 
 
-</article><!--MAIN CONTENT-->
 
 
-<?php include "includes/footer.php" ?>
+	</article>
+	<!--MAIN CONTENT-->
+
+
+	<?php include "includes/footer.php" ?>
