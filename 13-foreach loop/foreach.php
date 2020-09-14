a83d29626169140e24e0c0218248190dd137e4b1
Mahmoud Aboelsoud <mahmoud.aboelsoud.p@gmail.com>
2020-09-14 20:01:30 +0200

    13-foreach loop

---
+++ 13-foreach loop/foreach.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8>">
+    <title>Foreach loop</title>
+</head>
+
+<body>
+
+    <?php
+
+    echo "<h1>";
+
+
+    $names = array("Mahmoud", "Sami", "Ali");
+
+    foreach ($names as $name) {
+        echo $name . "  ";
+    }
+
+
+
+    echo "</h1>";
+    ?>
+
+
+</body>
+
+</html>
