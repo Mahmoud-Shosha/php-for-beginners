9fd3444766c45b12a32a998a2aaca348fe36a7cf
Mahmoud Aboelsoud <mahmoud.aboelsoud.p@gmail.com>
2020-09-14 19:54:50 +0200

    12- for loop

---
+++ 12-for loop/for.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8>">
+    <title>For loop</title>
+</head>
+
+<body>
+
+    <?php
+
+    echo "<h1>";
+
+
+    for ($counter = 0; $counter < 10; $counter++) {
+        echo $counter . "  ";
+    }
+
+
+
+    echo "</h1>";
+    ?>
+
+
+</body>
+
+</html>
