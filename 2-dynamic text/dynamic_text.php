39b02fd7e90e5b13f845718a640572c798fd752a
Mahmoud Aboelsoud <mahmoud.aboelsoud.p@gmail.com>
2020-09-14 15:44:04 +0200

    2-dynamic text

---
+++ 2-dynamic text/dynamic_text.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8>">
+    <title>Dynamic text</title>
+</head>
+
+<body>
+
+    <?php
+
+    $title = "Operating Systems";
+
+    // This is a single line comment
+
+    /*
+        This is a multi-line comment 
+    */
+
+    ?>
+
+    <h2><?php echo $title ?></h2>
+
+</body>
+
+</html>
