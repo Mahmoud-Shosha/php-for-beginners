76e2eb7048eded46574c69364fb660e7eaf3f1a5
Mahmoud Aboelsoud <mahmoud.aboelsoud.p@gmail.com>
2020-09-14 18:27:59 +0200

    8-if

---
+++ 8-if/if.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8>">
+    <title>If statements</title>
+</head>
+
+<body>
+
+    <?php
+
+    echo "<h1>";
+
+
+    $length = "LONG";
+
+    if ($length == "SHORT") {
+        echo "You are short !";
+    } elseif ($length == "LONG") {
+        echo "You are long !";
+    } else {
+        echo "UNDEFINED";
+    }
+
+
+
+
+
+    echo "</h1>";
+    ?>
+
+
+</body>
+
+</html>
