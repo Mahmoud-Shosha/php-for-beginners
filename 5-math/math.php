f3689dbd278803973ee47635f8ae918421926c8c
Mahmoud Aboelsoud <mahmoud.aboelsoud.p@gmail.com>
2020-09-14 17:07:04 +0200

    5-math

---
+++ 5-math/math.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8>">
+    <title>Math</title>
+</head>
+
+<body>
+
+    <?php
+
+    echo "<h1>";
+
+
+    echo 5 + 3;
+    echo "<br>";
+
+    echo 5 - 3;
+    echo "<br>";
+
+    echo 5 * 3;
+    echo "<br>";
+
+    echo 5 / 3;
+    echo "<br>";
+
+    echo "<br>";
+
+    echo 5 + 5 * 10;
+    echo "<br>";
+
+    echo (5 + 5) * 10;
+    echo "<br>";
+
+
+    echo "</h1>";
+    ?>
+
+
+</body>
+
+</html>
