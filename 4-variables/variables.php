ce3f6d6520b07e01a9027495f2020d8ba3ccc3b7
Mahmoud Aboelsoud <mahmoud.aboelsoud.p@gmail.com>
2020-09-14 16:52:56 +0200

    4-variables

---
+++ 4-variables/variables.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8>">
+    <title>Variables</title>
+</head>
+
+<body>
+
+    <?php
+
+    $name;
+    echo $name;
+
+    $name = "Mahmoud";
+    echo $name . "<br>";
+
+    $name = 'Mahmoud';
+    echo $name . "<br>";
+
+    $age = 25;
+    echo $age . "<br>";
+
+    $AGE = 88;  // case-senestive
+    echo $AGE . "<br>";
+
+    ?>
+
+
+</body>
+
+</html>
