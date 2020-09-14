bf471a31e139366e49ecb8a4af708f0616525223
Mahmoud Aboelsoud <mahmoud.aboelsoud.p@gmail.com>
2020-09-14 19:48:14 +0200

    11-while

---
+++ 11-while loop/while.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8>">
+    <title>While loop</title>
+</head>
+
+<body>
+
+    <?php
+
+    echo "<h1>";
+
+
+    $counter = 0;
+
+    while ($counter < 10) {
+        echo $counter . "  ";
+        // $counter = $counter + 1;
+        $counter++;
+    }
+
+
+
+    echo "</h1>";
+    ?>
+
+
+</body>
+
+</html>
