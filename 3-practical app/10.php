7f631ae7820061808e430e319f2c53cb64661259
Mahmoud Aboelsoud <mahmoud.aboelsoud.p@gmail.com>
2020-09-16 03:13:38 +0200

    3-practical app

--- 3-practical app/10.php
+++ 3-practical app/10.php
@@ -4,37 +4,63 @@
 
 	<aside class="col-xs-4">
 
-		<?php Navigation();?>
-			
-			
-	</aside><!--SIDEBAR-->
+		<?php Navigation(); ?>
 
-<article class="main-content col-xs-8">
 
-	
-	<?php  
+	</aside>
+	<!--SIDEBAR-->
 
-	/*  Step 1: Use the Make a class called Dog
+	<article class="main-content col-xs-8">
 
-		Step 2: Set some properties for Dog, Example, eye colors, nose, or fur color
 
-		Step 4: Make a method named ShowAll that echos all the properties
+		<?php
 
-		Step 5: Instantiate the class / create object and call it pitbull
+		/* 
+			Step 1: Use the Make a class called Dog
 
-Step 6: Call the method ShowAll
+			Step 2: Set some properties for Dog, Example, eye colors, nose, or fur color
 
-	
+			Step 4: Make a method named ShowAll that echos all the properties
 
+			Step 5: Instantiate the class / create object and call it pitbull
+
+			Step 6: Call the method ShowAll
 		
-	*/
-	
-	?>
+		*/
+
+		class Dog
+		{
+			public $eye_color;
+			public $nose_color;
+			public $fur_color;
+
+			public function __construct($eye_color, $nose_color, $fur_color)  // can not be protected
+			{
+				$this->eye_color = $eye_color;
+				$this->nose_color = $nose_color;
+				$this->fur_color = $fur_color;
+			}
+
+			public function show_all()
+			{
+				$result =  "Eye color is " . $this->eye_color . ", ";
+				$result .= "Nose color is " . $this->nose_color . ", ";
+				$result .= "Fur color is " . $this->fur_color . "<br>";
+				echo $result;
+			}
+		}
+
+
+		$pitbull = new Dog("red", "green", "blue");
+		$pitbull->show_all();
+
+		?>
 
 
 
 
 
-</article><!--MAIN CONTENT-->
+	</article>
+	<!--MAIN CONTENT-->
 
-<?php include "includes/footer.php" ?>
+	<?php include "includes/footer.php" ?>
